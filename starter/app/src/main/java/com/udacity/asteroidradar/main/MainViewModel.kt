cae602ca9396940891c5e4bd6096d25ba49bb59e
OMERIYIOZ <omer.iyioz@turkcell.com.tr>
2021-01-16 08:29:39 +0300

    improved

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,34 +1,25 @@
 package com.udacity.asteroidradar.main
 
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.udacity.asteroidradar.network.AsteroidApi
-import com.udacity.asteroidradar.network.MarsApi
-import com.udacity.asteroidradar.network.MarsProperty
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-/*    private val _properties = MutableLiveData<List<MarsProperty>>()
-
-    // The external LiveData interface to the property is immutable, so only this class can modify
-    val properties: LiveData<List<MarsProperty>>
-        get() = _properties*/
-
     private val _properties = MutableLiveData<ImageOfTheDay>()
 
-    // The external LiveData interface to the property is immutable, so only this class can modify
     val properties: LiveData<ImageOfTheDay>
         get() = _properties
 
     init {
         getImageOfTheDay()
     }
-    fun getImageOfTheDay(){
+
+    fun getImageOfTheDay() {
         viewModelScope.launch {
             try {
                 _properties.value = AsteroidApi.RETROFIT_SERVICE.getProperties()
@@ -37,12 +28,5 @@ class MainViewModel : ViewModel() {
             }
         }
 
-       /* viewModelScope.launch {
-            try {
-                _properties.value = MarsApi.retrofitService.getProperties()
-            } catch (e: Exception) {
-                Log.d("omertest", "getImageOfTheDay: 123 + $e")
-            }
-        }*/
     }
 }
--- starter/app/src/main/java/com/udacity/asteroidradar/network/AsteroidApiService.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/network/AsteroidApiService.kt
@@ -1,11 +1,9 @@
 package com.udacity.asteroidradar.network
 
-// import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.main.ImageOfTheDay
 import okhttp3.OkHttpClient
-// import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
